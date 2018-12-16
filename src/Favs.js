9549a74fea1755213d0769fa29aec3b7973fe196
Scott <scottfuller4@gmail.com>
2018-12-16 16:43:48 -0500

    fixed bug causing the page to crash if no strains have been saved to favourites

--- src/Favs.js
+++ src/Favs.js
@@ -1,42 +1,48 @@
 import React, { Component } from 'react';
 
 class Favs extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
 
         }
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <section className={this.props.showSection ? 'favStrains' : 'hidden'}>
-                <div className="wrapper">
-                    <h2>Favourite strains</h2>
-                    <div className="strainContainer favStrainsContainer">
-                        {
-                            Object.entries(this.props.strainList).map((strain) => {
-                                return (
-                                    <div className="strain" key={strain[0]}>
-                                        <h3>{strain[1].name}</h3>
-                                        <p>Positive Effects:</p>
-                                        <ul>
-                                            {
-                                                strain[1].effects.map((effect) => {
-                                                    return (
-                                                        <li key={effect}>{effect}</li>
-                                                    )
-                                                })
-                                            }
-                                        </ul>
-                                        <p>Race: {strain[1].race}</p>
-                                        <button onClick={this.props.deleteStrain} id={strain[0]}><i class="fas fa-times"></i></button>
-                                    </div>
-                                )
-                            })
-                        }
+                {this.props.strainList ?
+                    <div className="wrapper">
+                        <h2>Favourite strains</h2>
+                        <div className="strainContainer favStrainsContainer">
+                            {
+                                Object.entries(this.props.strainList).map((strain) => {
+                                    return (
+                                        <div className="strain" key={strain[0]}>
+                                            <h3>{strain[1].name}</h3>
+                                            <p>Positive Effects:</p>
+                                            <ul>
+                                                {
+                                                    strain[1].effects.map((effect) => {
+                                                        return (
+                                                            <li key={effect}>{effect}</li>
+                                                        )
+                                                    })
+                                                }
+                                            </ul>
+                                            <p>Race: {strain[1].race}</p>
+                                            <button onClick={this.props.deleteStrain} id={strain[0]}><i class="fas fa-times"></i></button>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div> :
+                    <div classname="wrapper">
+                        <h2>Favourite strains</h2>
+                        <h3>You haven't added any favourite strains yet.</h3>
                     </div>
-                </div>
+                }
             </section>
         )
     }
